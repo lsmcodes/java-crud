4287725586f3331be08e488527e1697aab2bcbea
Lorena Medeiros <lomedeiros@proton.me>
2024-10-02 10:11:01 -0300

    feat: add Course model to represent a course in the system

---
+++ src/main/java/io/github/lsmcodes/crud_java/model/course/Course.java
@@ -0,0 +1,32 @@
+package io.github.lsmcodes.crud_java.model.course;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String category;
+
+}
